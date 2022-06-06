2c0ad2ba991c378f28f08b95cb062f622151a2b8
FlorentineC <72718503+FlorentineC@users.noreply.github.com>
2022-06-06 14:32:35 +1000

    Add files via upload

---
+++ artworkInformation.js
@@ -0,0 +1,13 @@
+//Artwork Information
+//exporting arrays from another javascript file
+//source: https://www.tutorialrepublic.com/faq/how-to-include-a-javascript-file-in-another-javascript-file.php
+//filename, x, y, z, title, description, background colour, ambient light colour, 
+export const artworklist = [
+    ['window2.glb',0.01,0.3,2.4,'#1 Window','this is a description',"rgb(18, 20, 26)"],
+    ['cafe2.glb',3.6,1.1,-2.6,'#2 Cafe Scene','this is a description',"rgb(38, 36, 36)"],
+    //Vector3 {x: 3.36809352955974, y: 1.6933962084160634, z: -3.1746425401074547}
+    //['window2.glb',1.8,1.6,4.3,'title', 'description'],
+    ['bus.glb', 6.3,0.1,0.1,'#4 Bus', '','rgb(73, 105, 105)'],
+    ['balcony2.glb', 1.8, 1.6, 4.3,
+    'Balcony Scene','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'],
+];
